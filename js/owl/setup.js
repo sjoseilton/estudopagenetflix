ab3732af799b5c6edf8c7edfc4c2067277b67888
Joseilton Silva <sjoseilton@gmail.com>
2020-11-17 22:07:57 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:5
+        },
+        1000:{
+            items:10
+        }
+    }
+})
